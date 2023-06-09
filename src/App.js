b89a4ee4e5dfadd03c9d5b3708393afbbe761ba8
JOSH <joshuadoe168@gmail>
2023-06-09 12:56:11 +0000

    second commit

--- src/App.js
+++ src/App.js
@@ -1,50 +1,64 @@
+import "./App.css";
+import TopButton from "./WeatherComp/TopButton"
+import Inputs from "./WeatherComp/Inputs";
+import TimeAndLocation from "./TimeAndLocation"
+import TemperatureAndDetails from "./TemperatureAndDetails";
+import Forecast from "./Forecast";
+import getFormattedWeatherData from "./services/weatherService";
+import { useEffect, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-import './App.css';
-import UilReact from '@iconscout/react-unicons/icons/uil-react'
-import TopButton from './WeatherComp/TopButton';
-import Inputs from './WeatherComp/Inputs';
-import TimeAndLocation from './TimeAndLocation';
-import TemperatureAndDetails from './TemperatureAndDetails';
-import Forecast from './Forecast';
-import getFormattedWeatherData from './services/weatherService';
-import { useEffect, useState } from 'react';
 function App() {
+  const [query, setQuery] = useState({ q: "berlin" });
+  const [units, setUnits] = useState("metric");
+  const [weather, setWeather] = useState(null);
 
-const [query, setQuesry] =  useState ({q: 'berlin'})
-const [units, setUnits] =  useState ('metric')
-const [weather, setWeather] =  useState ('null')
+  useEffect(() => {
+    const fetchWeather = async () => {
+      const message = query.q ? query.q : "current location.";
 
-useEffect (() => {
-  const fetchWeather = async () => {
-     await getFormattedWeatherData({...query, units}).then(
-       (data) => {
-         setWeather(data);
-       });
-   
+      toast.info("Fetching weather for " + message);
+
+      await getFormattedWeatherData({ ...query, units }).then((data) => {
+        toast.success(
+          `Successfully fetched weather for ${data.name}, ${data.country}.`
+        );
+
+        setWeather(data);
+      });
+    };
+
+    fetchWeather();
+  }, [query, units]);
+
+  const formatBackground = () => {
+    if (!weather) return "from-cyan-700 to-blue-700";
+    const threshold = units === "metric" ? 20 : 60;
+    if (weather.temp <= threshold) return "from-cyan-700 to-blue-700";
+
+    return "from-yellow-700 to-orange-700";
   };
 
-fetchWeather();
-}, [query, units]);
-
-
   return (
-  <div className='mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl 
-  shadow-gray-400'>
-      <TopButton />
-     <Inputs />
+    <div
+      className={`mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br  h-fit shadow-xl shadow-gray-400 ${formatBackground()}`}
+    >
+      <TopButton setQuery={setQuery} />
+      <Inputs setQuery={setQuery} units={units} setUnits={setUnits} />
 
-     {weather && (
-       <div>
-         <TimeAndLocation weather={weather}/>
-         <TemperatureAndDetails weather={weather}/>
-         <Forecast title="hourly forecast" />
-         <Forecast title="daily forecast" />
-         </div>
-     )}
+      {weather && (
+        <div>
+          <TimeAndLocation weather={weather} />
+          <TemperatureAndDetails weather={weather} />
 
-   
-  </div>
+          <Forecast title="hourly forecast" items={weather.hourly} />
+          <Forecast title="daily forecast" items={weather.daily} />
+        </div>
+      )}
 
+      <ToastContainer autoClose={5000} theme="colored" newestOnTop={true} />
+    </div>
   );
 }
 
--- src/WeatherComp/TopButton.jsx
+++ src/WeatherComp/TopButton.jsx
@@ -1,44 +1,42 @@
-import React from 'react'
+import React from "react";
 
-function TopButton() {
- const cities=[
-     {
-         id:1,
-         title: "Ghana"
-     },
-     {
-        id:2,
-        title: "London"
+function TopButton({ setQuery }) {
+  const cities = [
+    {
+      id: 1,
+      title: "London",
     },
     {
-        id:3,
-        title: "Toronto"
+      id: 2,
+      title: "Sydney",
     },
     {
-        id:4,
-        title: "Paris"
+      id: 3,
+      title: "Tokyo",
     },
     {
-        id:5,
-        title: "Togo"
+      id: 4,
+      title: "Toronto",
     },
     {
-        id:6,
-        title: "Sydney"
+      id: 5,
+      title: "Paris",
     },
+  ];
 
- ]
-
-
-
-
-
-
-  return <div className='flex items-center justify-around my-6'>
-      {cities.map((city) =>(
-          <button key={city.id} className='text-white text-lg font-medium'>{city.title}</button>
+  return (
+    <div className="flex items-center justify-around my-6">
+      {cities.map((city) => (
+        <button
+          key={city.id}
+          className="text-white text-lg font-medium"
+          onClick={() => setQuery({ q: city.title })}
+        >
+          {city.title}
+        </button>
       ))}
-  </div>
-};
+    </div>
+  );
+}
 
 export default TopButton;
